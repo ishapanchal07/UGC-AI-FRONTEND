7623af8e8e9af290e31ad4cc6709ecd191e30b70
ishapanchal07 <isha7112003@gmail.com>
2026-02-07 16:57:12 +0530

    creating a generator

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -1,7 +1,32 @@
+import React, { useState } from "react"
 import Title from "../components/Title"
+import UploadZone from "../components/UploadZone"
 
 
 const Generator = () => {
+
+    const [name, setName] = useState('')
+    const [productName, setProductName] = useState('')
+    const [productDescription, setProductDescription] = useState('')
+    const [aspectRatio, setAspectRatio] = useState('9:16')
+    const [productImage, setProductImage] = useState<File | null>(null)
+    const [modelImage, setModelImage] = useState<File | null>(null)
+    const [userprompt, setUserPrompt] = useState('')
+    const [isGenerating, setIsGenerating] = useState(false)
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'product' | 'model') => {
+        if(e.target.files && e.target.files[0]){
+            if(type === 'product') setProductImage(e.target.files[0]);
+            else setModelImage(e.target.files[0])
+        }
+    }
+
+    const handleGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
+
+    }
+    
+
+
     return (
         <div className="min-h-screen text-white p-6 md:p-12 mt-28">
             <form className="max-w-4xl mx-auto mv-40">
@@ -9,8 +34,7 @@ const Generator = () => {
                 <div className="flex gap-20 max-sm:flex-col items-start justify-between">
                     {/* left col */}
                     <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-                        <p>product Image</p>
-                        <p>model Image</p>
+                        <UploadZone label="Product Image" file={} onClear={} onChange={}/>
                     </div>
                     {/* right col */}
                     <div>
