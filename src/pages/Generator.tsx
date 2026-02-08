3b2cfd88f4f4d038a623db8b5357b36e5455ff0e
ishapanchal07 <isha7112003@gmail.com>
2026-02-08 12:30:12 +0530

    community generating

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -13,7 +13,7 @@ const Generator = () => {
     const [productImage, setProductImage] = useState<File | null>(null)
     const [modelImage, setModelImage] = useState<File | null>(null)
     const [userprompt, setUserPrompt] = useState('')
-    const [isGenerating, setIsGenerating] = useState(true)
+    const [isGenerating, setIsGenerating] = useState(false)
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'product' | 'model') => {
         if (e.target.files && e.target.files[0]) {
@@ -36,49 +36,63 @@ const Generator = () => {
                 <div className="flex gap-20 max-sm:flex-col items-start justify-between">
                     {/* left col */}
                     <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-                       <UploadZone label="Product Image" file={productImage} onClear={()=>setProductImage(null)} onChange={(e)=>handleFileChange(e, 'product')} />
-                        <UploadZone label="Model Image" file={modelImage} onClear={()=>setModelImage(null)} onChange={(e)=>handleFileChange(e, 'model')} />
+                        <UploadZone label="Product Image" file={productImage} onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
+                        <UploadZone label="Model Image" file={modelImage} onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
                     </div>
 
                     {/* right col */}
                     <div className="w-full">
                         <div className="mb-4 text-gray-300">
                             <label htmlFor="name" className="block text-sm mb-4">Project Name</label>
-                            <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Name your project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name your project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all" />
                         </div>
                         <div className="mb-4 text-gray-300">
                             <label htmlFor="name" className="block text-sm mb-4">Product Name</label>
-                            <input type="text" id="productName" value={productName} onChange={(e)=>setProductName(e.target.value)} placeholder="Name your product" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                            <input type="text" id="productName" value={productName} onChange={(e) => setProductName(e.target.value)} placeholder="Name your product" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all" />
                         </div>
                         <div className="mb-4 text-gray-300">
                             <label htmlFor="productDescription" className="block text-sm mb-4">Product Description <span className="text-xs text-violet-400">(optional)</span></label>
-                            <textarea id="productDescription" rows={4} value={productDescription} onChange={(e)=>setProductDescription(e.target.value)} placeholder="Enter the description of the product" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-nonetransition-all"/>
+                            <textarea
+                                id="productDescription"
+                                rows={4}
+                                value={productDescription}
+                                onChange={(e) => setProductDescription(e.target.value)}
+                                placeholder="Enter the description of the product"
+                                className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"
+                            />
                         </div>
 
                         <div className="mb-4 text-gray-300">
                             <label className="block text-sm mb-4">Aspect Ratio</label>
                             <div className="flex gap-3">
-                                <RectangleVerticalIcon onClick={()=>setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '9:16' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
-                                <RectangleHorizontalIcon onClick={()=>setAspectRatio('16:9')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '16:9' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
+                                <RectangleVerticalIcon onClick={() => setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '9:16' ? 'ring-violet-500/50 bg-white/10' : ''}`} />
+                                <RectangleHorizontalIcon onClick={() => setAspectRatio('16:9')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '16:9' ? 'ring-violet-500/50 bg-white/10' : ''}`} />
                             </div>
                         </div>
                         <div className="mb-4 text-gray-300">
                             <label htmlFor="userPrompt" className="block text-sm mb-4">User Prompt <span className="text-xs text-violet-400">(optional)</span></label>
-                            <textarea id="userprompt" rows={4} value={userprompt} onChange={(e)=>setUserPrompt(e.target.value)} placeholder="Describe how you want the narration to be." className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-nonetransition-all"/>
+                            <textarea
+                                id="userprompt"
+                                rows={4}
+                                value={userprompt}
+                                onChange={(e) => setUserPrompt(e.target.value)}
+                                placeholder="Describe how you want the narration to be."
+                                className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"
+                            />
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center mt-10"> 
+                <div className="flex justify-center mt-10">
                     <PrimaryButton disabled={isGenerating} className="px-10 py-3 rounded-md disabled:opacity-70 disabled:cursor-not-allowed">
                         {isGenerating ? (
                             <>
-                            <Loader2Icon className="size-5 animate-spin"/> Generating...
-                        </>
-                    ) : (
-                    <>
-                    <Wand2Icon className="size-5"/> Generate Image
-                    </>
-                    )}
+                                <Loader2Icon className="size-5 animate-spin" /> Generating...
+                            </>
+                        ) : (
+                            <>
+                                <Wand2Icon className="size-5" /> Generate Image
+                            </>
+                        )}
                     </PrimaryButton>
                 </div>
             </form>
