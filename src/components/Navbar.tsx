7b8c6e606721c382faadee3b46f722d31fe6c5b5
ishapanchal07 <isha7112003@gmail.com>
2026-02-16 14:43:43 +0530

    added userauthentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
-import { MenuIcon, XIcon } from 'lucide-react';
-import { PrimaryButton } from './Buttons';
+import { DollarSign, GalleryHorizontalEnd, MenuIcon, SparkleIcon, XIcon } from 'lucide-react';
+import { GhostButton, PrimaryButton } from './Buttons';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
 
 export default function Navbar() {
+
+    const navigate = useNavigate()
+    const {user} = useUser()
+    const {openSignIn, openSignUp} = useClerk()
+
     const [isOpen, setIsOpen] = useState(false);
 
     const navLinks = [
@@ -35,16 +41,37 @@ export default function Navbar() {
                     ))}
                 </div>
 
+                {!user ? (
                 <div className='hidden md:flex items-center gap-3'>
-                    <button className='text-sm font-medium text-gray-300 hover:text-white transition max-sm:hidden'>
+                    <button onClick={()=> openSignIn()} className='text-sm font-medium text-gray-300 hover:text-white transition max-sm:hidden'>
                         Sign in
                     </button>
-                    <PrimaryButton className='max-sm:text-xs hidden sm:inline-block'>Get Started</PrimaryButton>
+                    <PrimaryButton onClick={()=> openSignUp()} className='max-sm:text-xs hidden sm:inline-block'>Get Started</PrimaryButton>
                 </div>
+                ) : (
+                    <div className='flex  gap-2'>
+                        <GhostButton onClick={()=> navigate('/pland')} className='border-none text-gray-300 sm:py-1.5'>
+                            Creadits:
+                        </GhostButton>
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='Generate' labelIcon={<SparkleIcon size={14} />} onClick={()=>navigate('/generate')}/>
 
+                                <UserButton.Action label='My Generations' labelIcon={<SparkleIcon size={14} />} onClick={()=>navigate('/my-generations')}/>
+
+                                <UserButton.Action label='Community' labelIcon={<GalleryHorizontalEnd size={14} />} onClick={()=>navigate('/community')}/>
+
+                                <UserButton.Action label='Plans' labelIcon={<DollarSign size={14} />} onClick={()=>navigate('/plans')}/>
+                            </UserButton.MenuItems>
+                        </UserButton>
+                    </div>
+                )}
+
+                {!user && 
                 <button onClick={() => setIsOpen(!isOpen)} className='md:hidden'>
                     <MenuIcon className='size-6' />
                 </button>
+                }
             </div>
             <div className={`flex flex-col items-center justify-center gap-6 text-lg font-medium fixed inset-0 bg-black/40 backdrop-blur-md z-50 transition-all duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
                 {navLinks.map((link) => (
@@ -53,10 +80,10 @@ export default function Navbar() {
                     </a>
                 ))}
 
-                <button onClick={() => setIsOpen(false)} className='font-medium text-gray-300 hover:text-white transition'>
+                <button onClick={() => {setIsOpen(false); openSignIn()}} className='font-medium text-gray-300 hover:text-white transition'>
                     Sign in
                 </button>
-                <PrimaryButton onClick={() => setIsOpen(false)}>Get Started</PrimaryButton>
+                <PrimaryButton onClick={() => {setIsOpen(false); openSignUp()}}>Get Started</PrimaryButton>
 
                 <button
                     onClick={() => setIsOpen(false)}
