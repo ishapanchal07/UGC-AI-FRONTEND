1672628e62d062e4cb1f41e16dbc44dab76a15fb
ishapanchal07 <isha7112003@gmail.com>
2026-02-16 13:53:18 +0530

    creating a plan page

--- src/pages/Plans.tsx
+++ src/pages/Plans.tsx
@@ -1,9 +1,11 @@
+import Pricing from "../components/Pricing"
 
 
 const Plans = () => {
     return (
-        <div>
-
+        <div className="max-sm:py-10 sm:pt-20">
+            <Pricing />
+            <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create stunning images for just 5 creadits and generate immersive videos for 10 creadits.</p>
         </div>
     )
 }
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -18,6 +18,10 @@ const Result = () => {
         }, 3000)
     }
 
+    const handleGenerateVideo = async ()=>{
+        setIsGenerating(true)
+    }
+
     useEffect(()=>{
         fetchProjectData()
     }, [])
@@ -82,12 +86,17 @@ const Result = () => {
                             <h3 className="text-xl font-semibold mb-2">Video Magic</h3>
                             <p className="text-gray-400 text-sm mb-6">Turn this static image into a dynamic video for social media.</p>
                             {!project.generatedVideo ? (
-                                <PrimaryButton>
-                                    <SparkleIcon className="size-4"/>
-                                    Generated Video
+                                <PrimaryButton onClick={handleGenerateVideo}
+                                disabled={isGenerating} className="w-full">
+                                    {isGenerating ? (
+                                        <>Generating Video...</>
+                                    ) : (
+                                        <><SparkleIcon className="size-4"/>Generated Video</>
+                                    )}
+                                    
                                 </PrimaryButton>
                             ) : (
-                                <div className="p-3 bg-green-500/10 border border-green-500/20 rounded-xl text-green-500/20 rounded-xl ">
+                                <div className="p-3 bg-green-500/10 border border-green-500/20 rounded-xl text-green-400 text-center text-sm font-medium">
                                     Video Generated Successfully!!
                                 </div>
                             )}
