7b52fcbc2b5611e2fe7921f464b799b3641a5f4e
ishapanchal07 <isha7112003@gmail.com>
2026-02-16 14:00:47 +0530

    loading page created

--- src/pages/Plans.tsx
+++ src/pages/Plans.tsx
@@ -5,7 +5,7 @@ const Plans = () => {
     return (
         <div className="max-sm:py-10 sm:pt-20">
             <Pricing />
-            <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create stunning images for just 5 creadits and generate immersive videos for 10 creadits.</p>
+            <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create stunning images for just <span className="text-indigo-400 font-medium">5 creadits</span> and generate immersive videos for <span className="text-indigo-400 font-medium">10 creadits</span>.</p>
         </div>
     )
 }
