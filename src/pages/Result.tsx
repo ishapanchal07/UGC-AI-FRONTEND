8b80a7492f763f39bb4401f471e4a9db9e038989
ishapanchal07 <isha7112003@gmail.com>
2026-02-26 14:44:02 +0530

    added function

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,25 +1,55 @@
 import { useEffect, useState } from "react"
 import type { Project } from "../types"
-import { dummyGenerations } from "../assets/assets"
 import { ImageIcon, Loader2Icon, RefreshCwIcon, SparkleIcon, VideoIcon } from "lucide-react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { GhostButton, PrimaryButton } from "../components/Buttons"
+import { useAuth, useUser } from "@clerk/clerk-react"
+import toast from "react-hot-toast"
+import api from "../configs/axios"
 
 
 const Result = () => {
+
+    const {projectId} = useParams()
+    const {getToken} = useAuth()
+    const {user, isLoaded} = useUser()
+    const navigate = useNavigate()
+
     const [project, setProjectData] = useState<Project>({} as Project)
     const [loading, setLoading] = useState(true)
     const [isGenerating, setIsGenerating] = useState(false)
 
     const fetchProjectData = async () =>{
-        setTimeout(() =>{
-            setProjectData(dummyGenerations[0])
-            setLoading(false)
-        }, 3000)
+        try {
+            const token = await getToken()
+            const {data} = await api.get(`/api/user/projects/${projectId}`, {
+                headers: { Authorization: `Bearer ${token}`}
+            })
+            setProjectData(data.project)
+            setIsGenerating(data.project,isGenerating)
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message);
+            console.log(error);
+        }
     }
 
     const handleGenerateVideo = async ()=>{
         setIsGenerating(true)
+        try {
+            const token = await getToken();
+            const { data } = await api.post('/api/project/video', {projectId}, {
+                headers: { Authorization: `Bearer ${token}`}
+            })
+            setProjectData(prev => ({...prev, generatedVideo: data.videoUrl, 
+            isGenerating:false}))
+
+            toast.success(data.message); 
+            setIsGenerating(false);
+
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message);
+            console.log(error);
+        }
     }
 
     useEffect(()=>{
